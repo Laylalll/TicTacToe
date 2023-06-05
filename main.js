42f7e3fc39c806b0c6d06362cf31b88d3d17a8a7
Laylalll <yusioulai@gmail.com>
2023-06-05 17:19:35 +0800

    check winning player or tied

--- main.js
+++ main.js
@@ -17,13 +17,33 @@ const checkingLines = [
   [1, 5, 9],
   [3, 5, 7],
 ];
+let gameOverFlag = false //初始值，控制遊戲推進
 
 // function
+function checkWinningCondition(player) {
+  const winningPlayer = isPlayerWin(positions[player])
+  if (winningPlayer) {
+    gameOverFlag = true
+    return alert(`${player} player won !`)
+  }
+
+  if (getEmptyPositions().length === 0) {
+    gameOverFlag = true
+    return alert('Tied !')
+  }
+}
+
+function getEmptyPositions() {
+  const allOccupiedPositions = positions['circle'].concat(positions['cross'])
+  const allPositions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  return allPositions.filter((position) => !allOccupiedPositions.includes(position))
+}
+
 function isPlayerWin(checkingPositions) {
   // checkingPositions = [1, 4, 3, 2]
   for (const line of checkingLines) {
     // line = [1, 2, 3]
-    if (line.every((position) => { checkingPositions.includes(position) })) {
+    if (line.every((position) => checkingPositions.includes(position))) {
       return true
     }
   }
@@ -37,15 +57,20 @@ function draw(position, shape) {
 
 function onCellClicked(event) {
   const position = Number(event.target.dataset.index);
+
   if (count % 2 === 0) {
     draw(position, 'circle')
     positions['circle'].push(position)
-    console.log('circle', positions['circle'])
+
+    setTimeout(() => checkWinningCondition('circle'), 500)
+
   } else {
     draw(position, 'cross')
     positions['cross'].push(position)
-    console.log('cross', positions['cross'])
+
+    setTimeout(() => checkWinningCondition('cross'), 500)
   }
+
   count++
 }
 
